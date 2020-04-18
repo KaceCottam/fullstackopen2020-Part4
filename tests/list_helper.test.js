8a74b2133566c2480b34d7950b4b6adfeecd69f1
KaceCottam <kace.cottam@wsu.edu>
2020-04-18 12:58:57 -0700

    Exercise 4.4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,8 +1,52 @@
-const listHelper = require('../utils/list_helper')
+const { dummy, totalLikes } = require('../utils/list_helper')
 
 test('dummy returns one', () => {
   const blogs = []
 
-  const result = listHelper.dummy(blogs)
+  const result = dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  test('of an empty list is zero', () => {
+    const blogs = []
+
+    const result = totalLikes(blogs)
+    expect(result).toBe(0)
+  })
+  test('when list has only one blog equals the likes of that', () => {
+    const blogs = [
+      {
+        title: "Hello, world!",
+        author: "Kace",
+        url: "www.google.com",
+        likes: 5,
+        id: "5e9b560d99a17f1c8ed08d69"
+      }
+    ]
+
+    const result = totalLikes(blogs)
+    expect(result).toBe(5)
+  })
+  test('of a bigger list is calculated right', () => {
+    const blogs = [
+      {
+        title: "Hello, world!",
+        author: "Kace",
+        url: "www.google.com",
+        likes: 5,
+        id: "5e9b560d99a17f1c8ed08d69"
+      },
+      {
+        title: "Something",
+        author: "Kace",
+        url: "www.example.com",
+        likes: 10,
+        id: "5e9a560d99a17f1c8ed08d69"
+      }
+    ]
+
+    const result = totalLikes(blogs)
+    expect(result).toBe(15)
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,9 @@ const dummy = blogs => {
  return 1
 }
 
-module.exports = { dummy }
+const totalLikes = blogs => {
+  const sum = (x, y) => x + y
+  return blogs.map(b => b.likes).reduce(sum, 0)
+}
+
+module.exports = { dummy, totalLikes }

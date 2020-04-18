926c593a4769153186e66f2f6cb21b475c3e0fbd
KaceCottam <kace.cottam@wsu.edu>
2020-04-18 12:09:35 -0700

    Exercise 4.1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -20,13 +20,19 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/api/blogs', (_, res) => {
-  Blog.find({}).then(blogs => res.json(blogs))
+  Blog
+    .find({})
+    .then(blogs => res.json(blogs))
+    .catch(error => res.json(error))
 })
 
 app.post('/api/blogs', (req, res) => {
   const blog = new Blog(req.body)
 
-  blog.save().then(result => res.status(201).json(result))
+  blog
+    .save()
+    .then(result => res.status(201).json(result))
+    .catch(error => res.json(error))
 })
 
 const PORT = process.env.PORT || 3003
